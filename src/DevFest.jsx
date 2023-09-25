df33e2e3b2882cd688cfd897ea7869d7c3ad309c
aalvaropc <aalvaropc@gmail.com>
2023-09-25 18:42:06 -0500

    Added Location View

--- src/DevFest.jsx
+++ src/DevFest.jsx
@@ -1,12 +1,14 @@
 import Principal from "./Views/Principal";
 import Speakers from "./Views/Speakers";
 import Acerca from "./Views/Acerca";
+import Ubicanos from "./Views/Ubicanos"
 function App() {
   return (
     <>
       <Principal></Principal>
       <Speakers></Speakers>
       <Acerca></Acerca>
+      <Ubicanos></Ubicanos>
     </>
   );
 }
---
+++ src/components/Ubicanos/Texto/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Text.css";
+
+const Texto = () => {
+  return (
+    <h2 className="font__container md:texto__md lg:texto__lg md:text__center">
+      Ubicanos
+    </h2>
+  );
+};
+
+export default Texto;
