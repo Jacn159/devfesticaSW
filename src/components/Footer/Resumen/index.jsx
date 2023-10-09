983cd67c9ef00369944eefea434f201286d903d8
aalvaropc <aalvaropc@gmail.com>
2023-10-09 18:49:44 -0500

    add location

--- src/components/Footer/Resumen/index.jsx
+++ src/components/Footer/Resumen/index.jsx
@@ -4,7 +4,7 @@ const Resumen = () => {
     <div className="resumen">
       <h1 className="resumen__titulo">Resumen del evento</h1>
       <ol className="resumen__lista">
-        <li className="resumen__lista__item">Lugar: Por definir</li>
+        <li className="resumen__lista__item">Lugar: Local Central - Universidad Nacional San Luis Gonzaga</li>
         <li className="resumen__lista__item">Fecha: 02 de Diciembre 2023 </li>
         <li className="resumen__lista__item">Hora: 09:00 am</li>
       </ol>
